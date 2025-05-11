9da624ff80b70046e56a67b641191a78cb80659c
Arseni Salavei <arsenisoloveii@gmail.com>
2025-05-11 17:59:03 +0200

    feat: product_warehouse request dto

---
+++ apbd-lab09/Models/DTOs/ProductWarehouseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_lab09.Models.DTOs;
+
+public class ProductWarehouseDTO
+{
+    public int ProductId { get; set; }
+    public int WarehouseId { get; set; }
+    public int Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
